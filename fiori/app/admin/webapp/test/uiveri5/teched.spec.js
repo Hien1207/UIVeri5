7055ccf5cbc594fb99074a4156ab8ae4bbd111d0
{hien} <{hientt@bap.jp}>
2022-10-19 11:38:55 +0700

    first commit

--- fiori/app/admin/webapp/test/uiveri5/teched.spec.js
+++ fiori/app/admin/webapp/test/uiveri5/teched.spec.js
@@ -2,8 +2,21 @@ require("./pages/manageBooksPage");
 require("./pages/newBookPage");
 
 describe("teched", function () {
-  it("<describing test scenario>", function () {
-    //When.onThe<>Page.iDoAction();
-    //Then.onThe<>Page.iShouldSeeSomething();
+	const bookTitle = "New Book";
+	it("create a new book", function () {
+		When.onTheManageBooksPage.iClickOnCreateNewBook();
+		When.onTheManageBooksPage.enterIDNewBook(1);
+		When.onTheManageBooksPage.iClickContinueButton();
+		When.onTheNewBookPage.iEnterTitle(bookTitle);
+		When.onTheNewBookPage.iSelectGenre();
+		When.onTheNewBookPage.iSelectAuthor();
+		When.onTheNewBookPage.iClickOnSaveButton();
+		Then.onTheNewBookPage.iSeeEditButton();
   });
+	it("should check book is added", function() {
+		When.onTheNewBookPage.iNavigateBack();
+		Then.onTheManageBooksPage.iSeeGoButton();
+		When.onTheManageBooksPage.iClickGoButton();
+		Then.onTheManageBooksPage.theBookListContains(bookTitle);
+	})
 });
